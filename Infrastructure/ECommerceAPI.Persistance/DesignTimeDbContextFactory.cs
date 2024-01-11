321e936748a00eab46bf8df788bd8188813e24d8
Ruken Afşin <rukenafsin13@gmail.com>
2024-01-11 23:08:56 +0300

    Project Created

---
+++ Infrastructure/ECommerceAPI.Persistance/DesignTimeDbContextFactory.cs
@@ -0,0 +1,23 @@
+﻿using ECommerceAPI.Persistance.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using System.Threading.Tasks;
+using ETicaretAPI.Persistence;
+
+namespace ECommerceAPI.Persistance
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<ECommerceAPIDbContext>
+    {
+        public ECommerceAPIDbContext CreateDbContext(string[] args)
+        {
+            DbContextOptionsBuilder<ECommerceAPIDbContext> dbContextOptionsBuilder = new();
+            dbContextOptionsBuilder.UseSqlServer(Configuration.ConnectionString);
+            return new(dbContextOptionsBuilder.Options);
+        }
+    }
+}
---
+++ Infrastructure/ECommerceAPI.Persistance/ServiceRegistration.cs
@@ -0,0 +1,21 @@
+﻿
+using ECommerceAPI.Persistance.Context;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ETicaretAPI.Persistence;
+
+namespace ECommerceAPI.Persistance
+{
+    public static class ServiceRegistration
+    {
+        public static  void AddPersistanceServices(this IServiceCollection services)
+        {
+            services.AddDbContext<ECommerceAPIDbContext>(options => options.UseSqlServer(Configuration.ConnectionString));
+        }
+    }
+}
